4afe38487d903c039b429e578c7e6f4eb4968a67
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-02-08 16:19:23 -0300

    add React Slick and change font family

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Slider from "react-slick";
+import { HeaderContent } from "./HeaderContent";
+
 const weekDays = [
     "DOM",
     "SEG",
@@ -9,26 +13,57 @@ const weekDays = [
 ];
 
 export function Content() {
+    const [active, setActive] = useState("DOM");
+
+    const settings = {
+        // dots: true,
+        // autoplay: true,
+        // fade: true,
+        // infinite: true,
+        // speed: 500,
+        // autoplaySpeed: 5000,
+        // slidesToShow: 3,
+        // slidesToScroll: 1
+    };
+
     return (
-        <div className="max-w-5xl mx-auto mt-16">
-            <div className="text-center">
-                <h1
-                    className="inline-block relative px-14 py-2 text-4xl font-extrabold border border-black before:absolute before:w-2 before:h-[40%] before:right-full before:top-[30%] before:bg-black after:absolute after:w-2 after:h-[40%] after:left-full after:top-[30%] after:bg-black"
-                >
-                    CINEMA
-                </h1>
-            </div>
+        <div className="max-w-4xl mx-auto mt-16">
+            <HeaderContent />
 
-            <div className="mt-16 mb-8 text-center">
-                <h1 className="text-4xl font-extrabold">PROGRAMAÇÃO</h1>
-            </div>
-
-            <div className="flex justify-center mt-16 pb-12 border-b-2 gap-4 text-4xl font-bold">
+            <div className="flex justify-center mt-16 pb-12 border-b-2 gap-8 text-4xl font-black">
                 {weekDays.map(day => (
-                    <h1>
+                    <h1
+                        key={day}
+                        className={`cursor-pointer  ${active === day ? "text-blue-900" : "text-gray-400"}`}
+                        onClick={() => setActive(day)}
+                    >
                         {day}
                     </h1>
                 ))}
+            </div>
+
+            <div  className="text-white text-center">
+                <h2 className="text-black text-3xl font-bold my-16">Filmes</h2>
+                <Slider {...settings} className="h-[50vh]">
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>1</h3>
+                    </div>
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>2</h3>
+                    </div>
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>3</h3>
+                    </div>
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>4</h3>
+                    </div>
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>5</h3>
+                    </div>
+                    <div className="bg-black max-w-2xl h-[50vh]">
+                        <h3>6</h3>
+                    </div>
+                </Slider>
             </div>
         </div>
     )
---
+++ src/components/HeaderContent.jsx
@@ -0,0 +1,17 @@
+export function HeaderContent() {
+    return (
+        <div>
+            <div className="text-center">
+                <h1
+                    className="inline-block relative px-14 py-2 text-5xl font-black border border-black before:absolute before:w-2 before:h-[40%] before:right-full before:top-[30%] before:bg-black after:absolute after:w-2 after:h-[40%] after:left-full after:top-[30%] after:bg-black"
+                >
+                    CINEMA
+                </h1>
+            </div>
+
+            <div className="mt-16 mb-8 text-center">
+                <h1 className="text-5xl font-black">PROGRAMAÇÃO</h1>
+            </div>
+        </div>
+    )
+}
